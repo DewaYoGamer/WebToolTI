bc606cbee9899f268c8a43176ee635de3c85e0a5
Yoga <dewaanoc135@gmail.com>
2023-12-02 01:37:17 +0800

    update conn

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = mysqli_connect("localhost", "root","","DataMahasiswaTI");
+	$conn = mysqli_connect("db-mysql-sgp1-60947-do-user-14829821-0.c.db.ondigitalocean.com", "yoga","AVNS_aoIAXKErk987VLZntO7","DataMahasiswaTI");
 
 	if (!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
